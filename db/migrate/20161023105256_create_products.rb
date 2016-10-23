7888f0d0730d8fad94ddde715961a000cc85a529
natellus <natellus@yahoo.com>
2016-10-23 13:20:46 +0200

    Add images

--- db/migrate/20161023105256_create_products.rb
+++ db/migrate/20161023105256_create_products.rb
@@ -8,7 +8,7 @@ class CreateProducts < ActiveRecord::Migration
       t.boolean :is_spicy?
       t.boolean :is_veg?
       t.boolean :is_best_offer?
-      s.string :path_to_image
+      t.string :path_to_image
       t.timestamps
     end
 
