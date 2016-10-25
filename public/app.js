93475dee6e94b859389aa5d0e6cdf578ff399f18
natellus <natellus@yahoo.com>
2016-10-25 19:29:41 +0200

    Add add_to_cart function stab

--- public/app.js
+++ public/app.js
@@ -3,5 +3,15 @@ function something()
 	var x = window.localStorage.getItem('bbb');
 	x = x*1 + 1;
 	window.localStorage.setItem('bbb', x);
-	alert(x)
+	alert(x);
+};
+
+
+
+function add_to_cart()
+{
+
+
+  alert('Hello from function');
+
 }
