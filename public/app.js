f9c8939cf4dc49ced245e03016c5c6675d0382d6
natellus <natellus@yahoo.com>
2016-10-27 11:40:10 +0200

    Add cart_get_number_of_items

--- public/app.js
+++ public/app.js
@@ -10,8 +10,31 @@ function something()
 
 function add_to_cart(id)
 {
-  var key = 'product_' + id
+var key = 'product_' + id 
+
 var x = window.localStorage.getItem(key);
 x = x*1 + 1;
 window.localStorage.setItem(key, x);
+//number of items in the cart
+alert('Items in your cart: ' +cart_get_number_of_items());
+};
+
+function cart_get_number_of_items()
+{
+
+  var cnt = 0;
+  for(var i = 0; i < window.localStorage.length; i++)
+  {
+    var key = window.localStorage.key(i); // we get the key
+    var value = window.localStorage.getItem(key); // get value, ruby: hh[key] = value
+
+    if(key.indexOf('product_') == 0)
+   {
+
+    cnt = cnt + value*1;
+
+
+   }
+}
+return cnt;
 }
